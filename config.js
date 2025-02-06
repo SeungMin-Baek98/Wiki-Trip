44fb45539a68e8842370289e8bf9b7a057b16abd
SeungMin Baek <158240718+SeungMin-Baek98@users.noreply.github.com>
2025-02-06 18:30:13 +0900

    vercel 재배포 예정

--- config.js
+++
@@ -1,1 +0,0 @@
-export const API_URL = "https://trip-wiki-api.vercel.app/";
--- src/components/api.js
+++ src/components/api.js
@@ -1,4 +1,4 @@
-import { API_URL } from "../../config.js";
+const API_URL = "https://trip-wiki-api.vercel.app/";
 
 export const request = async (startIdx, region, sortBy, searchWord) => {
   try {
